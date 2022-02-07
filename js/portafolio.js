442a122bf0a55e753860e2c61c3f060f97808b8f
alfredorods <alfredorods>
2022-02-07 02:10:55 -0500

    ADDED: full portafolio

---
+++ js/portafolio.js
@@ -0,0 +1,329 @@
+const path_screenshots = "./img/screenshots/";
+const data_projects = [
+    {
+        "project":"gestionar",
+        "title":"Gestionar App",
+        "role":"Desarrollador Fullstack",
+        "status":"Activo",
+        "year":2022,
+        "platform":"Web,Android y Ios",
+        "technologies":"Node Js, Angular y Ionic",
+        "integrations":"Mercado Libre API, Woocommerce API, Shopify API y Firebase",
+        "description":"Gestionar es una App de logística y servicios de delivery para empresas que integra diferentes marketplace como Mercado libre, Woocommerce y Shopify para sincronizar ventas de diferentes proveedores y prestar un servicio de paquetería a los mismos. Todo desarrollado bajo un ambiente cloud con diferentes tecnologías tales como Node Js, Angular e Ionic. En este proyecto cumplí tareas de fullstack para realizar las diferentes integraciones con los diferentes proveedores de ecommerce por medio de diferentes microservicios.",
+        "screenshots":[
+            path_screenshots+"gestionar_01.png",    
+            path_screenshots+"gestionar_02.png",
+            path_screenshots+"gestionar_03.png",
+            path_screenshots+"gestionar_04.png",
+            path_screenshots+"gestionar_05.png",
+            path_screenshots+"gestionar_06.png",
+            path_screenshots+"gestionar_07.png",
+            path_screenshots+"gestionar_08.png"
+        ],
+        "links":[
+            {
+                "link":"https://play.google.com/store/apps/details?id=com.cronapis.gestionar",
+                "label":"Playstore parnerts"
+            },
+            {
+                "link":"https://play.google.com/store/apps/details?id=com.cronapis.gestionarcliente",
+                "label":"Playstore clientes"
+            },
+            {
+                "link":"https://apps.apple.com/pa/app/gestionar/id1590602760",
+                "label":"App Store clientes"
+            },
+            {   "link":"https://www.gestionarlogistica.com.ar/",
+                "label":"Web cliente"
+            }
+        ]
+    },
+    {
+        "project":"rutas",
+        "title":"Rutas Panamá App",
+        "role":"Desarrollador frontend - Angular",
+        "status":"Activo",
+        "year":2021,
+        "platform":"Web,Android y Ios",
+        "technologies":"Laravel, Angular y Ionic",
+        "integrations":"Oauth, Firebase push, Sentry y Pusher",
+        "description":"Rutas Panamá es una app que conecta los servicios de buses de Rutas Panamá con sus usuarios en tiempo real con el fin de que los mismos puedan acceder a la información base de las diferentes rutas y unidades que posee Rutas Panamá tales como horarios, información de conductores y ubicación en tiempo real. En este proyecto cumplí tareas de frontend para realizar integraciones con el API y las diferentes integraciones de terceros del app como Firebase, Pusher y Sentry.",
+        "screenshots":[
+            path_screenshots+"rutas_01.png",    
+            path_screenshots+"rutas_02.png",
+            path_screenshots+"rutas_03.png",
+            path_screenshots+"rutas_04.png",
+            path_screenshots+"rutas_05.png",
+            path_screenshots+"rutas_06.png",
+            path_screenshots+"rutas_07.png"
+        ],
+        "links":[
+            {
+                "link":"https://play.google.com/store/apps/details?id=com.cronapis.rutas&hl=es",
+                "label":"Playstore clientes"
+            },
+            {
+                "link":" https://apps.apple.com/pa/app/rutas-panam%C3%A1/id1575753010",
+                "label":"App store clientes"
+            }
+        ]
+    },
+    {
+        "project":"rtic",
+        "title":"Rtic Ecommerce",
+        "role":"Desarrollador Woocommerce",
+        "status":"Activo",
+        "year":2018,
+        "platform":"Web",
+        "technologies":"Woocommerce",
+        "integrations":"Paypal y CrediCorp Bank Panamá",
+        "description":"Ecommerce de productos de refrigerio de la reconocida marca de EEUU, Rtic Coolers con integración de pasarela de pagos para que los diferentes clientes de la marca puedan realizar transacciones por medio de tarjeta de crédito., .",
+        "screenshots":[
+            path_screenshots+"rtic_01.png",    
+            path_screenshots+"rtic_02.png",
+            path_screenshots+"rtic_03.png",
+            path_screenshots+"rtic_04.png"
+        ],
+        "links":[
+            {
+                "link":"https://rticcoolers.net",
+                "label":"Web"
+            }
+        ]
+    },
+    {
+        "project":"ebit",
+        "title":"Ebit App",
+        "role":"Desarrollador frontend - React Native",
+        "status":"Activo",
+        "year":2019,
+        "platform":"Web, Android e Ios",
+        "technologies":"React Native",
+        "integrations":"Paypal",
+        "description":"Ebit Market es una aplicación móvil para el uso y compra de productos y servicios con criptomonedas tales como Ethereum, Bitcoin y Litecoin junto con la stable coin de la plataforma el Ebit.",
+        "screenshots":[
+            path_screenshots+"ebit_01.png",    
+            path_screenshots+"ebit_02.png",
+            path_screenshots+"ebit_03.png",
+            path_screenshots+"ebit_04.png",
+            path_screenshots+"ebit_05.png"
+        ],
+        "links":[
+            {
+                "link":"https://play.google.com/store/apps/details?id=com.cronapis.ebitapp&hl=es",
+                "label":"Playstore clientes"
+            },
+            {
+                "link":"https://apps.apple.com/pa/app/ebit-market/id1502933702",
+                "label":"App Store clientes"
+            },
+            {   "link":"https://ebitmarket.com/",
+                "label":"Web cliente"
+            }
+        ]
+    },
+    {
+        "project":"togofy",
+        "title":"Togofy App",
+        "role":"Líder técnico",
+        "status":"Inactivo",
+        "year":2021,
+        "platform":"Web, Android e Ios",
+        "technologies":"Node Js (Sails) y Ionic",
+        "integrations":"Oauth, Apache Kafka, Visa Cybersource y Firebase",
+        "description":"TOGOFY es una aplicación en la cual los usuarios pueden registrarse en sus distintas opciones (Google, Facebook, entre otros) y puedan realizar sus solicitudes de servicio como compras a tiendas y solicitudes de envío de encomiendas desde la aplicación los cuales serán enviados a la puerta de su hogar por medio del domiciliario. Dicha aplicación cuenta con promociones y cupones para mejorar la experiencia de sus clientes. Adicional a ello se desarrollaron 3 aplicaciones para comercios afiliados, domiciliarios y administradores de plataforma.",
+        "screenshots":[
+            path_screenshots+"togofy_01.jpg",    
+            path_screenshots+"togofy_02.png",
+            path_screenshots+"togofy_03.png",
+            path_screenshots+"togofy_04.jpg",
+            path_screenshots+"togofy_05.png",
+            path_screenshots+"togofy_06.png",
+            path_screenshots+"togofy_07.png"
+        ],
+        "links":[
+            {
+                "link":"https://apps.apple.com/pa/app/gestionar/id1590602760",
+                "label":"App Store clientes"
+            },
+            {
+                "link":"https://apps.apple.com/pa/app/gestionar/id1590602760",
+                "label":"Play Store clientes"
+            }
+        ]
+    },
+    {
+        "project":"placita",
+        "title":"Mi Placita Marketplace",
+        "role":"Desarrollador Backend - Node",
+        "status":"En desarrollo",
+        "year":2021,
+        "platform":"Web",
+        "technologies":"Node Js (Express) y Angular",
+        "integrations":"Oauth, Paguelo Facil (Pasarela de pago)",
+        "description":"Mi Placita será un marketplace donde multiples vendedores pueden crear su tienda y en ella publicar su catalogo de productos con su respectivo inventario de productos.",
+        "screenshots":[
+            path_screenshots+"mkp_01.png",    
+            path_screenshots+"mkp_02.png",
+            path_screenshots+"mkp_03.png",
+            path_screenshots+"mkp_04.png",
+            path_screenshots+"mkp_05.png",
+            path_screenshots+"mkp_06.png",
+            path_screenshots+"mkp_07.png"
+        ],
+        "links":[
+            {
+                "link":"https://miplacita.shop",
+                "label":"Web"
+            }
+        ]
+    },
+    {
+        "project":"inbox",
+        "title":"Inbox App",
+        "role":"Desarrollador Fullstack",
+        "status":"Activa",
+        "year":2018,
+        "platform":"Web, Android e Ios",
+        "technologies":"Laravel, Angular y Ionic",
+        "integrations":"Sin integraciones de terceros",
+        "description":"Inbox es una app de logística internacional que busca conectar usuarios que quieran realizar sus compras en los Estados unidos, crearse un casillero y poder por medio del mismo recibir sus productos en Ciudad De Panamá por medio de la logística internacional de la empresa Inbox.",
+        "screenshots":[
+            path_screenshots+"ibx_01.png",    
+            path_screenshots+"ibx_02.png",
+            path_screenshots+"ibx_03.png",
+            path_screenshots+"ibx_04.png",
+            path_screenshots+"ibx_05.png",
+            path_screenshots+"ibx_06.png",
+            path_screenshots+"ibx_07.png"
+        ],
+        "links":[
+            {
+                "link":"https://inbox.com.pa",
+                "label":"Web"
+            },
+            {
+                "link":"https://play.google.com/store/apps/details?id=com.cronapis.inbox&hl=es_DO",
+                "label":"Playstore"
+            },
+            {
+                "link":"https://apps.apple.com/pa/app/inbox-panama/id1458274734",
+                "label":"App store"
+            }
+        ]
+    },
+    {
+        "project":"exponential",
+        "title":"Exponential Web",
+        "role":"Desarrollador frontend",
+        "status":"Activa",
+        "year":2020,
+        "platform":"Web",
+        "technologies":"Laravel y Angular",
+        "integrations":"Sin integraciones de terceros",
+        "description":"Exponential latam es una empresa que ofrece servicios educativos a diferentes usuarios en toda LATAM, exponential se encarga de conectar universidades prestigiosas con alumnos interesado en tomar carreras universitarias o post grados.",
+        "screenshots":[ 
+            path_screenshots+"exp_01.png",    
+            path_screenshots+"exp_02.png",
+            path_screenshots+"exp_03.png",
+            path_screenshots+"exp_04.png"
+        ],
+        "links":[
+            {
+                "link":"https://www.exponential.lat",
+                "label":"Web"
+            }
+        ]
+    },
+    {
+        "project":"elite",
+        "title":"Grupo Elite App",
+        "role":"Desarrollador Fullstack",
+        "status":"Activa",
+        "year":2019,
+        "platform":"Web y Android",
+        "technologies":"Laravel, Angular y Ionic",
+        "integrations":"Google Calendar.",
+        "description":"Grupo elite App es una herramienta de trabajo para dicha empresa en donde los colaboradores de la misma pueden acceder a un espacio de trabajo en donde gestionan sus actividades diarias, la app cuenta con multiples funcionalidades tales como listado de clientes, manejo de oportunidades de ventas, integración con google calendar y llamadas en app a los clientes de la empresa.",
+        "screenshots":[
+            path_screenshots+"elite_01.png",    
+            path_screenshots+"elite_02.png",
+            path_screenshots+"elite_03.png",
+            path_screenshots+"elite_04.png",
+            path_screenshots+"elite_05.png"
+        ],  
+        "links":[
+            {
+                "link":"https://play.google.com/store/apps/details?id=com.cronapis.appcall&hl=es",
+                "label":"Playstore"
+            }
+        ]
+    },
+    {
+        "project":"wageen",
+        "title":"Wageen App",
+        "role":"Desarrollador Fullstack",
+        "status":"Inactiva",
+        "year":2020,
+        "platform":"Web, Android e Ios",
+        "technologies":"Node Js (express) y React native",
+        "integrations":"Oauth, Socket Io, Google Maps, Firebase y Visa Cybersource.",
+        "description":"Wageen es una aplicación  móvil para solicitudes de servicio de transporte privado entre usuarios y conductores. Cuenta con funcionalidades en tiempo real que conectan a los usuarios de manera rápida y segura y aumentan la experiencia del usuario dentro de la aplicación.",
+        "screenshots":[
+            path_screenshots+"wageen_01.png",    
+            path_screenshots+"wageen_02.png",
+            path_screenshots+"wageen_03.png",
+            path_screenshots+"wageen_04.png",
+            path_screenshots+"wageen_05.png"
+        ],
+        "links":[
+
+        ]
+    }
+
+];
+
+
+
+$(".portafolio-link").on("click",function(){
+    const name = $(this).data("item");
+    const project = data_projects.find(function(data){
+        if(data.project==name) return data;
+    });
+    $('#project-description').modal('show');
+    $('#title-project').html(project.title);
+    $('#role-project').html(project.role);
+    $('#year-project').html(project.year);
+    $('#status-project').html(project.status);
+    $('#platform-project').html(project.platform);
+    $('#techs-project').html(project.technologies);
+    $('#integrations-project').html(project.integrations);
+    $('#description-project').html(project.description);
+    $('#links-project').html("");
+    $('#lightgallery').html("");
+    let links = "";
+    links+="<ul>"
+    for(let i=0; i<project.links.length; i++) {
+        links+="<li><a href='"+project.links[i].link+"' target='_blank'>"+project.links[i].label+"</a></li>";
+    }
+    links+="</ul>"
+    $('#links-project').html(links);
+
+    let images = "";
+    for(let i=0; i<project.screenshots.length; i++) {
+        images+="<a href="+project.screenshots[i]+" target='_blank'><img class='gallery-item' src='"+project.screenshots[i]+"'/></a>";
+    }
+
+    $('#lightgallery').html(images);
+
+    lightGallery(document.getElementById('lightgallery'), {
+        plugins: [lgZoom, lgThumbnail]
+    });
+
+    $("#lightgallery > a").on("click",function(){
+        $('#project-description').modal('hide');
+    });
+
+
+
+});
